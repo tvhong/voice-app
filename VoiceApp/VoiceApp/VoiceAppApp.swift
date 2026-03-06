bc8de73d776659640555e5b3a6900e4535f1acca
Vy Hong <shiweistg@gmail.com>
2026-03-06 05:04:27 +0000

    Initial commit

---
+++ VoiceApp/VoiceApp/VoiceAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  VoiceAppApp.swift
+//  VoiceApp
+//
+//  Created by Vy Hong on 3/6/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VoiceAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
